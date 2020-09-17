4aecf43e96940aacb558b184cae9feced1afd20c
Cankun Wang <flykun0620@gmail.com>
2020-09-16 22:58:48 -0400

    Add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
+  app.setGlobalPrefix('/iris3/api')
   const options = new DocumentBuilder()
     .setTitle('IRIS3 API')
-    .setBasePath('/iris3/api/')
     .setDescription('IRIS3 API application')
     .setVersion('1.0')
     .build()
 
   const document = SwaggerModule.createDocument(app, options)
-  SwaggerModule.setup('', app, document)
+  SwaggerModule.setup('/iris3/api/docs', app, document)
   await app.listen(9005)
 }
 bootstrap()
